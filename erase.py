e4338c7206283dd386730d085777ea20b11ca133
aniquelodewijkx1 <anique@hadrian.io>
2024-12-16 18:59:37 +0100

    prog

--- erase.py
+++ erase.py
@@ -1,11 +1,14 @@
 import copy
+import logging
 import random
 from abc import ABC, abstractmethod
 
 import numpy as np
-from poetry.console.commands import self
 
-from sudoku import Sudoku
+from utils import is_valid, find_empty_cell
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 LEVEL_MAP = {
     'easy': 36,
@@ -16,9 +19,10 @@ LEVEL_MAP = {
 
 
 class Eraser(ABC):
-    def __init__(self, board: np.ndarray, difficulty: str):
+    def __init__(self, board: np.ndarray, difficulty: str, subgrids: dict):
         self.puzzle = copy.deepcopy(board)
         self.difficulty = difficulty
+        self.subgrids = subgrids
 
 
     @abstractmethod
@@ -28,64 +32,57 @@ class Eraser(ABC):
 
 
     @staticmethod
-    def has_unique_solution(puzzle: np.ndarray) -> bool:
+    def has_unique_solution(puzzle: np.ndarray, subgrids: dict) -> bool:
         """ Check if a puzzle has one solution with recursive backtracking. """
-        solutions = 0
+        solutions = [0]
 
-        def solve(puzzle: np.ndarray, solutions: int) -> None:
-            if Sudoku.find_empty_cell(puzzle) is None:
-                solutions += 1
-                return
-            if solutions > 1:
+        def solve(puzzle: np.ndarray, solutions: list[int], subgrids: dict) -> None:
+            if solutions[0] > 1:
                 return
 
-            for row in range(9):
-                for col in range(9):
-                    if puzzle[row, col] == 0:
-                        for num in range(1, 10):
-                            if Sudoku.is_valid(row, col, num):
-                                puzzle[row, col] = num
-                                solve(puzzle, solutions)
-                                puzzle[row, col] = 0
+            empty = find_empty_cell(puzzle)
+            if empty is None:
+                solutions[0] += 1
+                return
+
+            row, col = empty
+            for num in range(1, 10):
+                number = {'value': num, 'row': row, 'col': col}
+                if is_valid(board=puzzle, subgrids=subgrids, num=number):
+                    puzzle[row, col] = num
+                    solve(puzzle, solutions, subgrids)
+                    puzzle[row, col] = 0
             return
 
-
-        solve(puzzle, solutions)
-        return solutions == 1
+        puzzle_copy = puzzle.copy()
+        solve(puzzle=puzzle_copy, solutions=solutions, subgrids=subgrids)
+        return solutions[0] == 1
 
 
 class BalancedEraser(Eraser, ABC):
     """ Erases a balanced number of cells from each subgrid."""
-    def __init__(self, board: np.ndarray, difficulty: str):
-        super().__init__(board, difficulty)
+    def __init__(self, board: np.ndarray, difficulty: str, subgrids: dict) -> None:
+        super().__init__(board, difficulty, subgrids)
 
 
     def erase(self) -> np.ndarray:
+        logger.info("Making puzzle...")
+
         num_to_remove = LEVEL_MAP[self.difficulty]
         remove_per_cell = num_to_remove // 9
         leftover = num_to_remove % 9
 
-        # define starting cell for all 9 subgrids
-        subgrid_starts = []
-        for i in range(0, 8, 3):
-            for j in range(0, 8, 3):
-                subgrid_starts.append((i, j))
-
-        # erase a balanced number of numbers in each cell
-        for subgrid_start in subgrid_starts:
+        for subgrid, cells in self.subgrids.items():
             erased = 0
-            row_start, col_start = subgrid_start
-
             while erased < remove_per_cell:
-                random_row = random.randint(row_start, row_start + 2)
-                random_col = random.randint(col_start, col_start + 2)
-                if val := self.puzzle[random_row][random_col] != 0:
-                    self.puzzle[random_row][random_col] = 0
+                rand_row, rand_col = random.choice(list(cells))
+                if (val := self.puzzle[rand_row][rand_col]) != 0:
+                    self.puzzle[rand_row][rand_col] = 0
                     # verify single solution or restore the cell
-                    if Eraser.has_unique_solution(self.puzzle):
+                    if Eraser.has_unique_solution(puzzle=self.puzzle, subgrids=self.subgrids):
                         erased += 1
                     else:
-                        self.puzzle[random_row][random_col] = val
+                        self.puzzle[rand_row][rand_col] = val
 
         erased = 0
         while erased < leftover:
--- sudoku.py
+++ sudoku.py
@@ -1,15 +1,20 @@
+import logging
 import random
+from collections import deque
 
 import inquirer
+import matplotlib.patches as patches
 import numpy as np
 from matplotlib import pyplot as plt
-from poetry.console.commands import self
 
-from erase import BalancedEraser, AutomorphicEraser
+from erase import BalancedEraser
+from utils import is_valid, find_empty_cell
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 ERASER_MAP = {
     "None (Standard)": BalancedEraser,
-    "Automorphic": AutomorphicEraser
 }
 
 
@@ -20,56 +25,25 @@ class Sudoku:
         self.difficulty = difficulty
         self.type = type
 
+        self.subgrids = self.generate_irregular_subgrids() if self.type == 'Irregular Subgrids' else self.generate_subgrids()
+
         puzzle = self.generate_sudoku()
-        self.plot(puzzle)
-
-
-    @staticmethod
-    def is_valid(board: np.ndarray, subgrids: dict[int, set], num: dict) -> bool:
-        """ Checks if number is present in same row, column, or subgrid.
-        - board: 9x9 numpy array populated with numbers
-        - subgrids: {1: ((x, y), (x2, y2), ...), 2: (...), ...}
-        - proposed_num: {"value": int 1-9, "row": x, "col": y}
-        """
-        value, row, col, subgrid = num["value"], num["row"], num["col"], num["subgrid"]
-        if value in board[row]:
-            return False
-        if value in board[:, col]:
-            return False
-
-        subgrid_coords = subgrids[subgrid]
-        subgrid_vals = [board[r][c] for r, c in subgrid_coords]
-        if value in subgrid_vals:
-            return False
-
-        return True
-
-
-    @staticmethod
-    def find_empty_cell(board) -> tuple | None:
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == 0:
-                    return i, j
-        return None
+        self.plot(puzzle, self.subgrids)
 
 
     def populate_board(self) -> bool:
-        if self.type == "Irregular":
-            subgrids = self.generate_irregular_subgrids()
-        else:
-            subgrids = self.generate_subgrids()
-
-        empty_cell = self.find_empty_cell(self.board)
+        empty_cell = find_empty_cell(self.board)
 
         if not empty_cell:
             return True
 
         row, col = empty_cell
+        numbers = list(range(1, 10))
+        random.shuffle(numbers)
 
-        for number in range(1, 10):
+        for number in numbers:
             num = {"value": number, "row": row, "col": col}
-            if self.is_valid(self.board, num, subgrids):
+            if is_valid(board=self.board, subgrids=self.subgrids, num=num):
                 self.board[row][col] = number
 
                 if self.populate_board():
@@ -79,8 +53,11 @@ class Sudoku:
 
         return False
 
+
     @staticmethod
     def generate_subgrids() -> dict[int, set]:
+        logger.info("Generating subgrids...")
+
         subgrids = {i: set() for i in range(1, 10)}
         subgrid_starts = []
         for i in range(0, 8, 3):
@@ -90,36 +67,35 @@ class Sudoku:
         for i, (r, c) in enumerate(subgrid_starts):
             for z in range(0, 3):
                 for y in range(0, 3):
-                    subgrids[i].add((r + z, c + y))
+                    subgrids[i + 1].add((r + z, c + y))
 
         return subgrids
 
+
     def generate_irregular_subgrids(self) -> dict[int, set[tuple]]:
         """ Generates a dictionary that maps 9 subgrids to a set of coordinates. """
+        logger.info("Generating irregular subgrids...")
+
         subgrids = {i: set() for i in range(1, 10)}
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         # seed cells for 9 subgrids
         seeds = random.sample([(row, col) for row in range(9) for col in range(9)], 9)
         for i, (row, col) in enumerate(seeds):
-            self.board[row][col] = i
-            subgrids[i].add((row, col))
+            subgrids[i+1].add((row, col))
 
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
+        print(f'subgrids: {subgrids}')
         # expand each subgrid iteratively
-        while any(len(subgrids[subgrid]) for subgrid in subgrids) < 9:
+        while any(len(subgrids[subgrid]) < 9 for subgrid in subgrids):
             for s in range(1, 10):
-                if len(subgrids[s]) == 9:
+                if len(subgrids[s]) >= 9:
                     continue
 
                 potential = set()
                 for (x, y) in subgrids[s]:
                     for dx, dy in directions:
                         nx, ny = x + dx, y + dy
-                        if (
-                                0 <= nx < 9 and
-                                0 <= ny < 9 and
-                                self.board[nx][ny] == 0):
+                        if 0 <= nx < 9 and 0 <= ny < 9 and self.board[nx][ny] == 0:
                             potential.add((nx, ny))
                 if not potential:
                     continue
@@ -127,13 +103,15 @@ class Sudoku:
                 cell = random.choice(list(potential))
                 self.board[cell[0]][cell[1]] = s
                 subgrids[s].add(cell)
+                print(subgrids)
 
         return subgrids
 
 
+
     def erase(self):
         eraser_class = ERASER_MAP[self.type]
-        eraser = eraser_class(self.board, self.difficulty)
+        eraser = eraser_class(self.board, self.difficulty, self.subgrids)
 
         puzzle = eraser.erase()
 
@@ -141,7 +119,9 @@ class Sudoku:
 
 
     @staticmethod
-    def plot(grid):
+    def plot(grid, subgrids):
+        logger.info("Plotting Sudoku...")
+
         fig, ax = plt.subplots(figsize=(6, 6))
         ax.set_xlim(0, 9)
         ax.set_ylim(0, 9)
@@ -149,13 +129,27 @@ class Sudoku:
         ax.set_yticks(np.arange(0, 10, 1))
         ax.grid(which='both', color='black', linewidth=1)
 
-        # Add thicker lines for subgrids
-        for i in range(0, 10):
-            if i % 3 == 0:
-                ax.axhline(i, color='black', linewidth=2.5)
-                ax.axvline(i, color='black', linewidth=2.5)
+        # bold lines around custom subgrids
+        for subgrid_id, cells in subgrids.items():
+            rows = [cell[0] for cell in cells]
+            cols = [cell[1] for cell in cells]
 
-        # Fill numbers
+            # determine bounding box
+            min_row, max_row = min(rows), max(rows)
+            min_col, max_col = min(cols), max(cols)
+
+            # Convert grid coordinates to plot coordinates
+            # invert grid coords to plot coords
+            lower_left = (min_col, 8 - max_row)
+            width = max_col - min_col + 1
+            height = max_row - min_row + 1
+
+            # create rectangle patch
+            rect = patches.Rectangle(lower_left, width, height,
+                                     linewidth=2.5, edgecolor='black', facecolor='none')
+            ax.add_patch(rect)
+
+        # Fill in the numbers
         for i in range(9):
             for j in range(9):
                 num = grid[i, j]
@@ -163,12 +157,15 @@ class Sudoku:
                     ax.text(j + 0.5, 9 - i - 0.5, str(num),
                             fontsize=20, ha='center', va='center')
 
+        # Remove tick labels
         ax.set_xticklabels([])
         ax.set_yticklabels([])
+
         plt.show()
 
 
     def generate_sudoku(self) -> np.ndarray:
+        logger.info("Populating board...")
         if self.populate_board():
             puzzle = self.erase()
             return puzzle
@@ -187,11 +184,8 @@ if __name__ == '__main__':
             choices=['None (Standard)', 'Irregular Subgrids'],
         ),
     ]
-    try:
-        answers = inquirer.prompt(questions)
-        sudoku = Sudoku(
-            difficulty=answers['difficulty'].lower(),
-            type=answers['type'])
 
-    except Exception:
-        print('🏜️ Run me from the terminal, thanks!')
+    answers = inquirer.prompt(questions)
+    sudoku = Sudoku(
+        difficulty=answers['difficulty'].lower(),
+        type=answers['type'])
